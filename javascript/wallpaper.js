c501cecda5e6c32e75eb4ebeac4ac21cc29021b0
Nickeniklas <savonhen@arcada.fi>
2026-01-06 10:44:36 +0200

    removed unwanted & old wallpapers

--- javascript/wallpaper.js
+++ javascript/wallpaper.js
@@ -6,10 +6,7 @@ function randomWallpaper() {
     const images = [
         "/images/barcelona-sky_view_lowRes.jpg",
         "/images/inkoon_saaristo_salmi.jpg",
-        "/images/wallpaper.png",
         "/images/arabianranta_spring_EDIT_compressed.jpg",
-        "/images/kabböleterassi_sunset.jpg",
-        "/images/kim_mökki.jpg",
         "/images/berlin1.jpg",
         "/images/berlin2.jpg",
         "/images/berlin3.jpg",
@@ -21,14 +18,14 @@ function randomWallpaper() {
         "/images/kalasatama-sompasaari.jpg",
         "/images/kabbole_sea.jpg",
         "/images/arabiankirjaston-puut.jpg",
-        "/images/kabbole_sea.jpg",
     ];
 
     const randomIndex = Math.floor(Math.random() * images.length);
     const selectedImage = baseUrl + images[randomIndex];
+	const selectedImageUrl = "url(" + selectedImage + ")";
 
-    document.querySelector('body').style.backgroundImage = "url(" + selectedImage + ")";
-    //console.log("todays background check: " + selectedImage)
+    document.querySelector('body').style.backgroundImage = selectedImageUrl;
+    console.log("Image URL: " + selectedImageUrl);
 }
 
 randomWallpaper();
